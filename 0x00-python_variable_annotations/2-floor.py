44c7c4df7ca4d8262e80413750b91b00d312698a
benrkia <fatimazahrae.benrkia@gmail.com>
2024-01-11 21:57:01 +0000

    Python - Variable Annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""function floor which takes a float n as argument and returns the floor of the float"""
+import math
+
+
+def floor(n: float) -> int:
+    """Returns Floot of a floar"""
+    return math.floor(n)
